84d5d62971cf1f11f820dc49e39bf90272bccdc7
Abhinav Roy <abhinavroy@pop-os.localdomain>
2020-04-17 01:14:25 +0530

    To test

--- devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++ devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/abhinavroy/robotics_learn/udacity_project2/src/ball_chaser/srv/DriveToTarget.srv")
+set(ball_chaser_SERVICE_FILES "/home/abhinavroy/robotics_learn/udacity_robotics_project4/src/ball_chaser/srv/DriveToTarget.srv")
